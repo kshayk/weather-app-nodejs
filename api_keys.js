d8783d7332bdbc199138751b60f188faebed6144
Unknown <kshayk0@gmail.com>
2018-10-22 16:15:14 +0300

    added api keys

---
+++ api_keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    google_maps_api: "AIzaSyDjUb-Y8YQu0ziy-B219qnJ3tjpip61Hfo",
+    weather_api: "f8ce6d274cc4cdbf7d0239e388ecdd56",
+    darksky_api: "65bc0fddfd66f0b837aedc933696f942"
+};
